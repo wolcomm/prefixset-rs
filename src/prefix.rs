0997ddda2a3908e47474780101d6159aeda20377
Ben Maddison <benm@workonline.africa>
2021-07-08 00:05:45 +0200

    optimize prefix structs

--- src/prefix.rs
+++ src/prefix.rs
@@ -1,7 +1,7 @@
 use std::ops::BitOrAssign;
 use std::str::FromStr;
 
-use ipnet::{PrefixLenError, Ipv4Net, Ipv6Net};
+use ipnet::{AddrParseError, PrefixLenError, Ipv4Net, Ipv6Net};
 use num::PrimInt;
 
 pub trait IpPrefixAggregate
@@ -20,41 +20,87 @@ where
     fn new_from(&self, lenth: u8) -> Result<Self, PrefixLenError>;
 }
 
-pub type Ipv4Prefix = Ipv4Net;
+#[derive(Clone, Copy, Debug)]
+pub struct Ipv4Prefix {
+    ipnet: Ipv4Net,
+    bits: u32,
+    length: u8,
+}
+
+impl From<Ipv4Net> for Ipv4Prefix {
+    fn from(ipnet: Ipv4Net) -> Self {
+        Self {
+            ipnet,
+            bits: ipnet.network().into(),
+            length: ipnet.prefix_len(),
+        }
+    }
+}
+
+impl FromStr for Ipv4Prefix {
+    type Err = AddrParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(s.parse::<Ipv4Net>()?.into())
+    }
+}
 
 impl IpPrefixAggregate for Ipv4Prefix {
     type AggrMap = u32;
     const MAX_LENGTH: u8 = 32;
 
     fn bits(&self) -> Self::AggrMap {
-        self.network().into()
+        self.bits
     }
 
     fn length(&self) -> u8 {
-        self.prefix_len()
+        self.length
     }
 
     fn new_from(&self, length: u8) -> Result<Self, PrefixLenError> {
-        Ok(Self::new(self.network(), length)?.trunc())
+        Ok(Ipv4Net::new(self.ipnet.network(), length)?.trunc().into())
     }
 }
 
-pub type Ipv6Prefix = Ipv6Net;
+#[derive(Clone, Copy, Debug)]
+pub struct Ipv6Prefix {
+    ipnet: Ipv6Net,
+    bits: u128,
+    length: u8,
+}
+
+impl From<Ipv6Net> for Ipv6Prefix {
+    fn from(ipnet: Ipv6Net) -> Self {
+        Self {
+            ipnet,
+            bits: ipnet.network().into(),
+            length: ipnet.prefix_len(),
+        }
+    }
+}
+
+impl FromStr for Ipv6Prefix {
+    type Err = AddrParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(s.parse::<Ipv6Net>()?.into())
+    }
+}
 
 impl IpPrefixAggregate for Ipv6Prefix {
     type AggrMap = u128;
     const MAX_LENGTH: u8 = 128;
 
     fn bits(&self) -> Self::AggrMap {
-        self.network().into()
+        self.bits
     }
 
     fn length(&self) -> u8 {
-        self.prefix_len()
+        self.length
     }
 
     fn new_from(&self, length: u8) -> Result<Self, PrefixLenError> {
-        Ok(Self::new(self.network(), length)?.trunc())
+        Ok(Ipv6Net::new(self.ipnet.network(), length)?.trunc().into())
     }
 }
 
